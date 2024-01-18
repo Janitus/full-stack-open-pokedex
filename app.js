deb2e5e4dc187c886d84a7a94fc72c6a4f831420
Jani <jankuul@gmail.com>
2024-01-18 19:29:25 +0200

    asd

--- app.js
+++ app.js
@@ -4,8 +4,4 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"))
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+app.use(express.static('dist'))
